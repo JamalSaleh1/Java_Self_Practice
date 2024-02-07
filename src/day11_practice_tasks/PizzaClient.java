bb19aa3a67e19dfbc8879440876371986be239f9
jamal <jamalsaleh518@gmail.com>
2024-02-06 21:17:05 -0600

    Pizza

---
+++ src/day11_practice_tasks/PizzaClient.java
@@ -0,0 +1,17 @@
+package day11_practice_tasks;
+
+public class PizzaClient {
+
+    public static void main(String[] args) {
+
+        Pizza pizza1 = new Pizza("small", 2,2);
+
+        Pizza pizza2 = new Pizza("Medium", 5,3);
+
+        Pizza pizza3 = new Pizza("Large", 1,1);
+
+        System.out.println(pizza1);
+        System.out.println(pizza2);
+        System.out.println(pizza3);
+    }
+}
