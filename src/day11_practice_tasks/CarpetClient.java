f21ab9558f1329b3c3c735da16590222bc19fae5
jamal <jamalsaleh518@gmail.com>
2024-02-09 09:11:33 -0600

    day 11

---
+++ src/day11_practice_tasks/CarpetClient.java
@@ -0,0 +1,17 @@
+package day11_practice_tasks;
+
+public class CarpetClient {
+
+    public static void main(String[] args) {
+
+        Carpet carpet1 = new Carpet(12,12,300,true);
+
+        Carpet carpet2 = new Carpet(15,27,200,false);
+
+        Carpet carpet3 = new Carpet(19,28,1880,true);
+
+        System.out.println(carpet1);
+        System.out.println(carpet2);
+        System.out.println(carpet3);
+    }
+}
